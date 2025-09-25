e33493fd01f76ba3e83366a2ba04240735c6dee3
Priya Mohan <priyavajram2303.com>
2025-09-25 10:35:50 +0530

    Updated file

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -7,25 +7,7 @@ const About = () => {
       title: "Integrity",
       description:
         "We prioritize honesty and transparency in all our dealings, ensuring a fair and ethical auction experience for everyone.",
-    },
-    {
-      id: 2,
-      title: "Innovation",
-      description:
-        "We continually enhance our platform with cutting-edge technology and features to provide users with a seamless and efficient auction process.",
-    },
-    {
-      id: 3,
-      title: "Community",
-      description:
-        "We foster a vibrant community of buyers and sellers who share a passion for finding and offering exceptional items.",
-    },
-    {
-      id: 4,
-      title: "Customer Focus",
-      description:
-        "We are committed to providing exceptional customer support and resources to help users navigate the auction process with ease.",
-    },
+    }
   ];
 
   return (
@@ -42,60 +24,6 @@ const About = () => {
             and bidding excitement. Founded in 2025. It provides a dynamic and user-friendly platform for buyers and
             sellers to connect, explore, and transact in a secure and seamless
             environment.
-          </p>
-        </div>
-        <div>
-          <h3
-            className={`text-[#111] text-xl font-semibold mb-2 min-[480px]:text-xl md:text-2xl lg:text-3xl`}
-          >
-            Our Mission
-          </h3>
-          <p className="text-xl text-stone-600">
-            At PrimeBid, our mission is to revolutionize the way people buy and
-            sell items online. We strive to create an engaging and trustworthy
-            marketplace that empowers individuals and businesses to discover
-            unique products, make informed decisions, and enjoy the thrill of
-            competitive bidding.
-          </p>
-        </div>
-        <div>
-          <h3
-            className={`text-[#111] text-xl font-semibold mb-2 min-[480px]:text-xl md:text-2xl lg:text-3xl`}
-          >
-            Our Values
-          </h3>
-          <ul className="list-inside">
-            {values.map((element) => {
-              return (
-                <li className="text-xl text-stone-600 mb-2" key={element.id}>
-                  <span className="text-black font-bold">{element.title}</span>:{" "}
-                  {element.description}
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-        
-          
-           
-        
-        <div>
-          <h3
-            className={`text-[#111] text-xl font-semibold mb-2 min-[480px]:text-xl md:text-2xl lg:text-3xl`}
-          >
-            Join Us
-          </h3>
-          <p className="text-xl text-stone-600">
-            Whether you're looking to buy, sell, or simply explore, PrimeBid
-            invites you to join our growing community of auction enthusiasts.
-            Discover new opportunities, uncover hidden gems, and experience the
-            thrill of winning your next great find.
-          </p>
-        </div>
-        <div>
-          <p className="text-[#d6482b] text-xl font-bold mb-3">
-            Thank you for choosing PrimeBid. We look forward to being a part of
-            your auction journey!
           </p>
         </div>
       </section>
